29ac8a62ff805e3ef128f5ea93a558b1fa51e47e
sindhu-123design <sindhuvattikonda@gmail.com>
2025-05-16 15:14:23 -0500

    correct syntax

--- src/ex_1_java_bascis/helloworld.java
+++ src/ex_1_java_bascis/helloworld.java
@@ -1,15 +1,13 @@
 package ex_1_java_bascis;
 
 public class helloworld {
-}
-
-     public static void main(string[] args)
 
 
-             system.out.println("Hello sindhu");
+    public static void main(String[] args)
+    {
 
-
+             System.out.println("Hello sindhu");
 
 
 }
-        }
+}
---
+++ src/ex_1_java_bascis/lab_01.java
@@ -0,0 +1,10 @@
+package ex_1_java_bascis;
+
+public class lab_01 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.printf("%d + %d",a,b);
+    }
+
+}
